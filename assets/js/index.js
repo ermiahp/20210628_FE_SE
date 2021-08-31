2bdee06fed9ad7282e507095f176b3b6fd443f63
ermiaHP <ermiahasanpour@gmail.com>
2021-08-31 11:22:21 +0200

    lesson 24

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+
+function showData() { 
+    let firstNameElemnt = document.getElementById("firstName");
+    let lastNameElemnt = document.getElementById("lastName");
+    let firstNameLabelElemnt = document.getElementById("firstNameLabel");
+    let lastNameLabelElemnt = document.getElementById("lastNameLabel");
+
+    firstNameLabelElemnt.innerText  += firstNameElemnt.value;
+    lastNameLabelElemnt.innerText  += lastNameElemnt.value;
+}
