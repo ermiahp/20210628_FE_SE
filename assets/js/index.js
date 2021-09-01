8baed229991547e92c68efc3b0d12f1a14d11575
“tobias365” <tobiasohman@outlook.com>
2021-09-01 18:13:19 +0200

    lesson24

---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+
+
+/*function showData() {
+  let firstNameElemnt = document.getElementById("firstName");
+  let firstNameLabelElemnt = document.getElementById("firstNameLabel")
+
+  firstNameLabelElemnt.innerText += firstNameElemnt.value;
+}
+*/
+/*
+const currentYear = 2021;
+
+function showData() {
+  let yearElemnt = document.getElementById("year");
+  let yearLabel = document.getElementById("yearLabel");
+
+  yearLabel.innerText += currentYear - yearElemnt.value; 
+}
+*/
+
+/*
+function showData() {
+  let oneSideElemnt = document.getElementById("oneSide");
+  let oneSideLabelElemnt = document.getElementById("oneSideLabel") 
+
+  oneSideLabelElemnt.innerText += oneSide.value * 4 + 'meters';
+
+}
+*/
+
+function showData() {
+  let howFarElemnt = document.getElementById("howFar");
+  let howLongElemnt = document.getElementById("howLong");
+  let howFastLabelElemnt = document.getElementById("howFastLabel");
+
+  howFastLabelElemnt.innerText += howFarElemnt.value / howLongElemnt.value +"KM/H";
+}
+
+
+//function showData() {
+  //let firstNameElemnt = document.getElementById("firstName");
+  //let lastNameElemnt = document.getElementById("lastName");
+  //let firstNameLabelElemnt = document.getElementById("firstNameLabel");
+  //let lastNameLabelElemnt = document.getElementById("lastNameLabel");
+
+  
+  //firstNameLabelElemnt.innerText += firstNameElemnt.value;
+  //lastNameLabelElemnt.innerText += lastNameElemnt.value;
+//}
+
