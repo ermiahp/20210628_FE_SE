95824a36d7850d4fe17d10ea06eccc8e7dfb6839
“tobias365” <tobiasohman@outlook.com>
2021-09-05 12:55:17 +0200

    lesson25

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,64 @@
 const resultBox = document.getElementsByClassName('calculator__display')[0];
 
 let firstPart = "";
+let secondPart = "";
+let selectedOperator = "";
 
 let clearCalc = function() {
   resultBox.innerHTML = 0;
   firstPart = "";
+  secondPart = "";
+  selectedOperator = "";
 }
 
 let pressNumber = function(digit) {
-  console.log(digit);
-  firstPart = firstPart + digit;
-  resultBox.innerHTML = firstPart;
+  if (secondPart.length + firstPart.length <= 12) {
+    if (selectedOperator === "") {
+      if (firstPart.length <= 8) {
+        firstPart = firstPart + digit;
+        resultBox.innerHTML = firstPart;
+      }
+    }else {
+      secondPart += digit;
+      resultBox.innerHTML += digit;
+    }
+  }
+}
+
+function pressOperator (operator) {
+  if (firstPart !== "" && secondPart === "" && selectedOperator === "") {
+    selectedOperator = operator;
+    resultBox.innerHTML += operator;
+  }
+}
+
+function showResult() {
+  switch (selectedOperator) {
+    case "+":
+        firstPart = (parseInt(firstPart) + parseInt(secondPart)).toString();
+        resultBox.innerHTML = firstPart;
+        selectedOperator = "";
+        secondPart = "";
+        break;
+    case "-":
+        firstPart = (parseInt(firstPart) - parseInt(secondPart)).toString();
+        resultBox.innerHTML = firstPart;
+        selectedOperator = "";
+        secondPart = "";
+        break;
+    case "*":
+        firstPart = (parseInt(firstPart) * parseInt(secondPart)).toString();
+        resultBox.innerHTML = firstPart;
+        selectedOperator = "";
+        secondPart = "";
+        break;  
+    case "/":
+        firstPart = (parseInt(firstPart) / parseInt(secondPart)).toString();
+        resultBox.innerHTML = firstPart;
+        selectedOperator = "";
+        secondPart = "";
+        break;
+    default:
+        break; 
+  }
 }
