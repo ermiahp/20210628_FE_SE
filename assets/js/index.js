314f52a6098c2f126fc1becb95ca389d473870f7
“tobias365” <tobiasohman@outlook.com>
2021-09-08 09:35:16 +0200

    a

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+/*
+var number = prompt("please enter a number between 1- 10");
+var sum = 0;
+
+for (var i = 0; i <= number; i++) {
+  sum = i + sum;
+}
+
+document.getElementById("demo").innerText = 'the result is : ' + sum;
+
+let a = 10;
+let b = 20;
+
+function number(a, b) {
+  if(a > b) {
+    return 1
+  }else if (a < b) {
+    return -1
+  }else if ()
+}
+*/
+var ageDiff = 0;
+
+function calculateYears() {
+  document.getElementById(age).innerText 
+  document.getElementById(mothersAge);
+
+  ageDiff = mothersAge - age;
+
+  document.getElementById(labelForYears).innerHTML = 'your mother was ' + ageDiff + ' when she gave birth to you.';
+}
